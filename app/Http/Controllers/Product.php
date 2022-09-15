f8d0abb0d452e9e19a28c193f667bbc70418c010
Kejsi Egro <kejsiegro@gmail.com>
2022-09-15 15:12:04 +0200

    product crud

--- app/Http/Controllers/Product.php
+++ app/Http/Controllers/Product.php
@@ -13,7 +13,7 @@ class Product extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -23,7 +23,7 @@ class Product extends Controller
      */
     public function create()
     {
-        //
+        return 1;
     }
 
     /**
